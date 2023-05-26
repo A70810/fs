e9bd4ac5bd8b9bd887686229b6163ea1519b8944
A70810 <ashutoshtripathi4567@gmail.com>
2023-05-26 18:29:56 +0530

    first-commit

---
+++ src/app/_models/user.ts
@@ -0,0 +1,10 @@
+﻿export class User {
+    id?: string;
+    username?:string;
+    email?:string;
+    password?: string;
+    name?: string;
+    lastName?: string;
+    token?: string;
+    user?:string;
+}
